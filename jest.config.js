7de9777ce70775b68b146ea21b7e5c3b2e8737ab
Ian Christian <ianmchristian@gmail.com>
2022-08-24 14:58:28 -0400

    added jsdom env

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
+  testEnvironment: 'jsdom',
   preset: 'ts-jest',
-  testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/'],
 };
