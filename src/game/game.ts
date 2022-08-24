04e2532dccfeaac69ed3198c13a48cfc5e784bbe
Ian Christian <ianmchristian@gmail.com>
2022-08-24 17:33:49 -0400

    Starting canvas grid

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,4 +1,5 @@
 import { Entity } from "../utils";
+import { Settings } from "../settings";
 
 export class Game extends Entity {
   public Entities: Entity[] =[]
@@ -16,7 +17,30 @@ export class Game extends Entity {
       this._lastTimestamp = Date.now()
       // begin loop
       this.Update()
+      this.DirtyDraw()
     })
+  }
+
+  private DirtyDraw(): void {
+    // Create and attach Canvas to DOM
+    const canvas = document.createElement('canvas')
+    const canvasSize = (Settings.grid.nodeSize + Settings.grid.nodeOffset) * Settings.grid.dimension + Settings.grid.nodeOffset
+    canvas.setAttribute('width', canvasSize.toString())
+    canvas.setAttribute('height', canvasSize.toString())
+    document.body.appendChild(canvas)
+
+    const size = Settings.grid.nodeSize
+    const offset = Settings.grid.nodeOffset
+    for (let y = 0; y < Settings.grid.dimension; y++) {
+      for (let x = 0; x < Settings.grid.dimension; x++) {
+        const ctx = canvas.getContext('2d')!
+        ctx.beginPath()
+        ctx.fillStyle = Settings.grid.color
+        ctx.rect((size + offset) * x, (size + offset) * y, size, size)
+        ctx.fill()
+      }
+    }
+
   }
 
   public Update(): void {
