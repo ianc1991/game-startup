ae9d9a6250821ab7ff3cc1bd1b1d07993aadfa59
Ian Christian <ianmchristian@gmail.com>
2022-08-26 16:11:06 -0400

    implements node to grid

--- src/grid/grid.ts
+++ src/grid/grid.ts
@@ -1,3 +1,28 @@
 import { Entity } from "../utils";
+import { Node } from "../node";
 
-export class Grid extends Entity { }
+export class Grid extends Entity {
+  private _nodes: Node[] = []
+
+  public get Nodes(): Node[] {
+    return this._nodes
+  }
+
+  public Awake(): void {
+    // awake componenets
+    super.Awake()
+    // awake children
+    for (const node of this._nodes) {
+      node.Awake()
+    }
+  }
+
+  public Update(deltaTime: number): void {
+    // update components
+    super.Update(deltaTime)
+    // update children
+    for (const node of this._nodes) {
+      node.Update(deltaTime)
+    }
+  }
+}
