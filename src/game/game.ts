00634f5538d012b80a24d6ee9f998c15061458a5
Ian Christian <ianmchristian@gmail.com>
2022-08-27 15:41:02 -0400

    refactored grid drawing

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,5 +1,4 @@
 import { Entity } from "../utils";
-import { Settings } from "../settings";
 import { Grid } from "../grid";
 
 export class Game extends Entity {
@@ -24,30 +23,7 @@ export class Game extends Entity {
       this._lastTimestamp = Date.now()
       // begin loop
       this.Update()
-      this.DirtyDraw()
     })
-  }
-
-  private DirtyDraw(): void {
-    // Create and attach Canvas to DOM
-    const canvas = document.createElement('canvas')
-    const canvasSize = (Settings.grid.nodeSize + Settings.grid.nodeOffset) * Settings.grid.dimension + Settings.grid.nodeOffset
-    canvas.setAttribute('width', canvasSize.toString())
-    canvas.setAttribute('height', canvasSize.toString())
-    document.body.appendChild(canvas)
-
-    const size = Settings.grid.nodeSize
-    const offset = Settings.grid.nodeOffset
-    for (let y = 0; y < Settings.grid.dimension; y++) {
-      for (let x = 0; x < Settings.grid.dimension; x++) {
-        const ctx = canvas.getContext('2d')!
-        ctx.beginPath()
-        ctx.fillStyle = Settings.grid.color
-        ctx.rect((size + offset) * x, (size + offset) * y, size, size)
-        ctx.fill()
-      }
-    }
-
   }
 
   public Update(): void {
--- src/node/components/draw/draw.ts
+++ src/node/components/draw/draw.ts
@@ -1,5 +1,6 @@
 import { Entity, IComponent } from '../../../utils';
 import { Node } from '../../../node'
+import { Settings } from '../../../settings';
 
 
 export class NodeDrawComponent implements IComponent {
@@ -10,6 +11,16 @@ export class NodeDrawComponent implements IComponent {
   }
 
   Awake(): void {
+    const canvas = document.createElement('canvas')
+    const canvasSize = (Settings.grid.nodeSize + Settings.grid.nodeOffset) * Settings.grid.dimension + Settings.grid.nodeOffset
+    canvas.setAttribute('width', canvasSize.toString())
+    canvas.setAttribute('height', canvasSize.toString())
+    document.body.appendChild(canvas)
 
+    const ctx = canvas.getContext('2d')!
+    ctx.beginPath()
+    ctx.fillStyle = Settings.grid.color
+    ctx.rect(this.Entity.Start.x, this.Entity.Start.y, this.Entity.Size.x, this.Entity.Size.y)
+    ctx.fill()
   }
 }
