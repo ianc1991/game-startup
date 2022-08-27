c52da671444786bf5dbdcf9a93f75e1e1c4df662
Ian Christian <ianmchristian@gmail.com>
2022-08-27 16:19:48 -0400

    added canvas utility

---
+++ src/utils/canvas/canvas.spec.ts
@@ -0,0 +1,58 @@
+import { Canvas } from "./canvas";
+import { Vector2D } from "../vector2D";
+
+describe('>>> Canvas', () => {
+  const size = new Vector2D(100, 100)
+  let canvas: Canvas
+
+  beforeEach(() => {
+    canvas = new Canvas(size)
+  })
+
+  it('should create and attach canvas to the DOM when awakens', () => {
+    const createElmSpy = jest.spyOn(document, 'createElement')
+    const appendChildSpy = jest.spyOn(document.body, 'appendChild')
+
+    expect(createElmSpy).not.toBeCalled()
+    expect(appendChildSpy).not.toBeCalled()
+
+    canvas.Awake()
+
+    expect(createElmSpy).toBeCalled()
+    expect(appendChildSpy).toBeCalled()
+  })
+
+  describe('>>> Canvas', () => {
+    beforeEach(() => {
+      canvas.Awake()
+    })
+
+    it('should draw and fill the rect', () => {
+      const start = new Vector2D(0, 0)
+      const size = new Vector2D(10, 10)
+      const color = '#ffff00'
+
+      const beginPathSpy = jest.spyOn(canvas.Context, 'beginPath')
+      const rectSpy = jest.spyOn(canvas.Context, 'rect')
+      const fillSpy = jest.spyOn(canvas.Context, 'fill')
+
+      canvas.FillRect(start, size, color)
+
+      expect(beginPathSpy).toBeCalled()
+      expect(rectSpy).toBeCalledWith(start.x, start.y, size.x, size.y)
+      expect(fillSpy).toBeCalled()
+      expect(canvas.Context.fillStyle).toBe(color)
+    })
+
+    it('should clear the rect', () => {
+      const start = new Vector2D(0, 0)
+      const size = new Vector2D(10, 10)
+
+      const spy = jest.spyOn(canvas.Context, 'clearRect')
+      expect(spy).not.toBeCalled()
+
+      canvas.ClearRect(start, size)
+      expect(spy).toBeCalledWith(start.x, start.y, size.x, size.y)
+    })
+  })
+})
